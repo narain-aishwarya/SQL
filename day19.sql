61c866a381a5e0848ddf90d33d38e04758e2b0f5
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-08-08 19:00:31 +0530

    1068. Product Sales Analysis I

---
+++ day19.sql
@@ -0,0 +1,17 @@
+/*+-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key of this table.
+product_id is a foreign key to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.*/
+SELECT p.product_name, s.year, s.price
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
