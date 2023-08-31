4891f5dfb4a8c095a7758d436556bdfea093a5a6
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-08-31 18:58:08 +0530

    610. Triangle Judgement

---
+++ day39.sql
@@ -0,0 +1,18 @@
+SELECT * ,
+  IF(x+y >z AND y + z >x AND z+x > y , "Yes", "No") AS triangle
+FROM Triangle
+
+/*
+Table: Triangle
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+Report for every three line segments whether they can form a triangle.
+Return the result table in any order.
+*/
