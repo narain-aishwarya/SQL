51a16b301d208483e7dd622feb34b4ceca0425f5
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-09-01 18:04:44 +0530

    1517. Find Users With Valid E-Mails

---
+++ day40.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM Users
+WHERE mail LIKE '[a-zA-Z]%@leetcode.com' AND LEFT(mail, LEN(mail) - 13) NOT LIKE '%[^0-9a-zA-Z_.-]%'
+/* This solution is for MS SQL Server,
+as MySQL not not support Like operator.*/
+
+/*Table: Users
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
+| mail          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains information of the users signed up in a website. Some e-mails are invalid.
+Write a solution to find the users who have valid emails.
+A valid e-mail has a prefix name and a domain where:
+The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+The domain is '@leetcode.com'.
+Return the result table in any order.*/
