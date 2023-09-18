90bcddaeb2a3206f287eaf4e034b31dfdd84931b
Aishwarya Narain <134004148+narain-aishwarya@users.noreply.github.com>
2023-09-18 22:05:34 +0530

    1174. Immediate Food Delivery II

---
+++ day56.sql
@@ -0,0 +1,7 @@
+with t as (select *, row_number() over (partition by customer_id order by order_date) as c
+, (case when order_date = customer_pref_delivery_date then 1.0 else 0 end) as 'immediate'
+from Delivery)
+
+select round(100*sum(immediate)/count(immediate),2) as 'immediate_percentage'
+from t
+where c=1
